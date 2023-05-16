4bd746747aae5b7668b62d461bc6223fb7433a24
varquezi <ivangvarquez@gmail.com>
2023-05-16 13:34:41 -0400

    Changed Footer Icon Styling

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,13 +11,13 @@ export default function Contact() {
         <section id="contact">
             <div className='contentm'>
             <div className='title c3'><span className='ul'>beam me up scotty</span> 🛸</div>
-            <div className='text f1 c3'>
+            <div className='text f1 c0'>
                 send me an email, let's get in touch!</div>
             </div>
             <div className='mailwrap'>
             <a href = "mailto: ivan.varquez@mail.utoronto.ca"><button>< FontAwesomeIcon icon={faEnvelope}  className='icon'/>send message</button></a>
             </div>
-            <footer className='f1 c3'>
+            <footer className='f1 c0'>
                 built with React by Ivan Varquez
                 <div className='linksf'>
                     <a href ="https://www.linkedin.com/in/ivanvarquez/" target="_blank">< FontAwesomeIcon icon={faLinkedin} className='icon' /></a>
