33300235a37a383a325379ea67f83524a0e9f9a7
varquezi <ivangvarquez@gmail.com>
2023-05-17 23:01:15 -0400

    Added tech property to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,13 +3,16 @@ import './comp.css'
 
 export default function Card(props: any){
     return(
-        <div className='card f1 c0' style={{backgroundImage: `url(${props.img})`}}>
+        <a href={props.link} target="_blank">
+        <div className='card f1 c0' style={{backgroundImage: `url(${props.img})`}}>        
             <div className="coverlay">
                 <div className="ccontent">
                     <div className='ctitle f1 c3 ul'>{props.name}</div>
                     <div className='ctext f1 c3'>{props.info}</div>
+                    <div className='ctech f1 it c3'>{props.tech}</div>
                 </div>
             </div>
         </div>
+        </a>
     )
 }
--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -4,8 +4,8 @@ import Card from './Card'
 
 export default function Showcase() {
     return(
-        <>
-            <Card name="OCT Scraper" info="i could never find the right way to tell you" img="https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg"/>
-        </>
+        <div className='showcase'>
+            <Card name="OCT Scraper" tech= "Python" link="https://github.com/varquezi/OCT_Scraper" info="i could never find the right way to tell you, have you noticed i've been gone" img="https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg"/>
+        </div>
     )
 }
