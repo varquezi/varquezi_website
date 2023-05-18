19c97d97a291beea5cde9b09249869d86a0c504e
varquezi <ivangvarquez@gmail.com>
2023-05-17 22:24:19 -0400

    Implemented Card Styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,15 @@
 import '../branding.css'
 import './comp.css'
 
-export default function Card(){
+export default function Card(props: any){
     return(
-        <div className='card f1 c0'>
-            hello
+        <div className='card f1 c0' style={{backgroundImage: `url(${props.img})`}}>
+            <div className="coverlay">
+                <div className="ccontent">
+                    <div className='ctitle f1 c3 ul'>{props.name}</div>
+                    <div className='ctext f1 c3'>{props.info}</div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -5,7 +5,7 @@ import Card from './Card'
 export default function Showcase() {
     return(
         <>
-            <Card />
+            <Card name="OCT Scraper" info="i could never find the right way to tell you" img="https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg"/>
         </>
     )
 }
