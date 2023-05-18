a5f1a613bb07707baa5f37b18d1177c8e4ae6b26
varquezi <ivangvarquez@gmail.com>
2023-05-18 00:57:36 -0400

    Added YouTube button

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 import './pages.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFile } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import resume from '../assets/varquezi_resume_may_2023.pdf'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
@@ -24,6 +24,7 @@ export default function Contact() {
                     <a href ="https://www.linkedin.com/in/ivanvarquez/" target="_blank">< FontAwesomeIcon icon={faLinkedin} className='icon' /></a>
                     <a href ="https://github.com/varquezi" target="_blank">< FontAwesomeIcon icon={faGithub} className='icon' /></a>
                     <a href ="https://www.instagram.com/ivanvarq/" target="_blank">< FontAwesomeIcon icon={faInstagram} className='icon' /></a>
+                    <a href ="https://www.youtube.com/@qarvingsi" target="_blank">< FontAwesomeIcon icon={faYoutube} className='icon' /></a>
                     <a href ={resume} target="_blank">< FontAwesomeIcon icon={faFile} className='icon' /></a>
                 </div>
             </footer>
