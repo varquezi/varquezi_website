3a5b29feb25fe1279d972f5b94db2249c6b57ba8
varquezi <ivangvarquez@gmail.com>
2023-05-19 19:42:56 -0400

    Updated Pictures

--- src/assets/projData.tsx
+++ src/assets/projData.tsx
@@ -4,7 +4,7 @@ export default [
         name: "Personal Website",
         tech: "TypeScript, React",
         link: "https://github.com/varquezi/varquezi_website",
-        img: "/src/assets/project_imgs/varquezi_website.jpg",
+        img: "/src/assets/proj_varquezi_website.webp",
         info: "You're looking at it! 😎 Basically built it to show off my projects and contact info (and that I can actually use React). All the pretty animations were done with the Framer Motion Package"
     },
     {
@@ -12,7 +12,7 @@ export default [
         name: "OCT Scraper",
         tech: "Python",
         link: "https://github.com/varquezi/OCT_Scraper",
-        img: "/src/assets/project_imgs/OCT_Scraper.jpg",
+        img: "/src/assets/proj_OCT_Scraper.webp",
         info: "Fetches the degrees of Ontario teachers from the public OCT Database and formats them as a CSV."
     },
 ]
