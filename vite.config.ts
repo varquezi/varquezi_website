e510e1e85d55fa9fb2ac46d198761c4bfa52a304
varquezi <ivangvarquez@gmail.com>
2023-05-20 18:20:30 -0400

    Changed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  base: "/varquezi_website/"
+  plugins: [react(), svgr()]
 })
